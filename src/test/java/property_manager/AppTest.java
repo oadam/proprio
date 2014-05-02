10c6df819edf676e8e496c2d425133a18ba6b4e7
oadam <oadam@efront.com>
2014-05-02 12:43:09 +0100

    Java8 + junit

---
+++ src/test/java/property_manager/AppTest.java
@@ -0,0 +1,19 @@
+package property_manager;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
