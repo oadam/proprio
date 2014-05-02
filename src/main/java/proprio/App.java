8d525277918fcc35b8fe70c1cebe0f597075afed
oadam <oadam@efront.com>
2014-05-02 15:29:43 +0100

    Sqlite and flyway

--- src/main/java/proprio/App.java
+++ src/main/java/proprio/App.java
@@ -1,17 +1,32 @@
 package proprio;
 
+import org.flywaydb.core.Flyway;
+
 /**
  * The main app of the application.
  *
  */
 public final class App {
-    /**Private to prevent instanciation.*/
+    /** Private to prevent instanciation. */
     private App() {
     }
 
-    /**The main method of the app.
-     * @param args Command line arguments*/
+    /**
+     * The main method of the app.
+     *
+     * @param args
+     *            Command line arguments
+     */
     public static void main(final String[] args) {
+        // Create the Flyway instance
+        Flyway flyway = new Flyway();
+
+        // Point it to the database
+        flyway.setDataSource("jdbc:sqlite:target/proprio.db", "sa", null);
+
+        // Start the migration
+        flyway.migrate();
+
         System.out.println("Hello World!");
     }
 }
