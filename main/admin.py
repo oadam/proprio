bf4d9f46dad900faf5c2798abf2f067bf2c5a5bd
oadam <oadam@efront.com>
2014-05-20 22:29:37 +0100

    Tenancy management

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.translation import ugettext as _
-from main.models import Building, Property
+from main.models import Building, Property, Tenancy, RentRevision
 
 class BuildingAdmin(admin.ModelAdmin):
     list_display = ('name', 'property_count')
@@ -8,6 +8,15 @@ class BuildingAdmin(admin.ModelAdmin):
 class PropertyAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'building')
 
+class RentRevisionInline(admin.TabularInline):
+    model = RentRevision
+
+class TenancyAdmin(admin.ModelAdmin):
+    inlines = [
+        RentRevisionInline
+    ]
+
 admin.site.register(Building, BuildingAdmin)
 admin.site.register(Property, PropertyAdmin)
+admin.site.register(Tenancy, TenancyAdmin)
 
--- main/models.py
+++ main/models.py
@@ -3,24 +3,43 @@ from django.utils.translation import ugettext_lazy as _
 
 class Building(models.Model):
     name = models.CharField(_("name"), max_length=255)
-
     class Meta:
         verbose_name = _("building")
-
     def __unicode__(self):
         return self.name
     def property_count(self):
         return self.property_set.count()
+    property_count.short_description = _("number of properties")
 
 class Property(models.Model):
     name = models.CharField(_("name"), max_length=255)
     building = models.ForeignKey(Building, verbose_name=Building._meta.verbose_name, blank=True, null=True, on_delete=models.PROTECT)
     address = models.TextField(_("address"))
-
     class Meta:
         verbose_name = _("property")
         verbose_name_plural = _("properties")
-
     def __unicode__(self):
         return self.name + '\n' + self.address
+
+class Tenancy(models.Model):
+    property = models.ForeignKey(Property, verbose_name=Property._meta.verbose_name, on_delete=models.PROTECT)
+    tenant_name = models.CharField(_("tenant name"), max_length=255)
+    tenant_email = models.EmailField(_("tenant email"), max_length=254, blank=True)
+    #begin date is inferred from first rent revision
+    end_date = models.DateField(_("end date"), blank=True, null=True)
+    class Meta:
+        verbose_name = _("tenancy")
+        verbose_name_plural = _("tenancies")
+    def __unicode__(self):
+        return self.tenant_name + ' ' + unicode(self.property)
+
+class RentRevision(models.Model):
+    tenancy = models.ForeignKey(Tenancy, verbose_name=Tenancy._meta.verbose_name, on_delete=models.PROTECT)
+    date = models.DateField(_("start date"))
+    rent = models.DecimalField(_("monthly rent"), max_digits=7, decimal_places=2)
+    provision = models.DecimalField(_("monthly provision"), max_digits=7, decimal_places=2)
+    class Meta:
+        verbose_name = _("rent revision")
+    def __unicode__(self):
+        return self.date
 
