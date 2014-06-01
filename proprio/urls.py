b71d2ae1f3f38f11fba7eb1d06ef65eecb6746c0
Olivier Adam <olivieradam666+spam@gmail.com>
2014-06-02 00:17:19 +0100

    Redirect to default page

--- proprio/urls.py
+++ proprio/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url='main/')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^main/', include('main.urls'))
+    url(r'^main/', include('main.urls')),
 )
