652aff7d45b3c43e1a1bf071b8ccb9263429ea62
Olivier Adam <olivieradam666+spam@gmail.com>
2014-06-02 21:20:20 +0100

    Integrated login form

--- main/views.py
+++ main/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from main.models import Tenant
 
 
+@login_required
 def tenants(request):
     tenant_list = Tenant.objects.all().order_by('name')
     context = {'tenant_list': tenant_list}
     return render(request, 'main/tenants.html', context)
 
 
+@login_required
 def tenant_cashflows(request, tenant_id):
     tenant = get_object_or_404(Tenant, pk=tenant_id)
     context = {'cashflows': tenant.cashflows()}
--- proprio/urls.py
+++ proprio/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.contrib.auth.views import logout
 
 admin.autodiscover()
 
@@ -8,4 +9,6 @@ urlpatterns = patterns('',
     url(r'^$', RedirectView.as_view(url='main/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^main/', include('main.urls')),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', lambda request: logout(request, '/')),
 )
